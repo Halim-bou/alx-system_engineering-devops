a708008bce0ae0efde847cd49e38b57d84a0f19d
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-26 16:53:34 -0800

    again

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # puppet_ssh_config.pp
 
 file_line { 'Turn off passwd auth':
-   path   => '~/.ssh/config',
+   ensure => present,
+   path   => 'etc/ssh/ssh_config',
    line   => 'PasswordAuthentication no',
-   match  => '^#?PasswordAuthentication.*',
 }
 
 file_line { 'Declare identity file':
-   path   => '~/.ssh/config',
+   ensure => present,
+   path   => 'etc/ssh/ssh_config',
    line   => 'IdentityFile ~/.ssh/school',
-   match  => '^#?IdentityFile.*',
 }
