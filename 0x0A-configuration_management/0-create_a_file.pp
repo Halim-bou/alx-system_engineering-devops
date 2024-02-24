9bbff824c00477590b2a123f8fcd31b0f3200ad8
aelbouaa <hbouaami@gmail.com>
2024-02-24 02:45:44 +0100

    creating file with CM puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create file using Puppet
-file {'tmp/school',
-path  => 'tmp/school',
+file {'tmp/school':
+path    => 'tmp/school',
 content => 'I love Puppet',
-owner => www-data,
-group => www-data,
-mode  =>'0744',
+owner   => www-data,
+group   => www-data,
+mode    =>'0744',
 }
