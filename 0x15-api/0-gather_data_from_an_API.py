ff0cff0370ebf47705f49461a5c67e6f766c7fa8
aelbouaa <hbouaami@gmail.com>
2024-03-25 23:10:49 +0000

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,6 @@ completed = f'{todo_list}?completed=True'
 non_completed = f'{todo_list}?completed=False'
 num_of_tasks = len(completed) + len(non_completed)
 user_data = requests.get(f'{data}{user_id}').json()
-todosData = requests.get(f'{data}{todo_list}').json()
 todosDone = requests.get(f'{data}{completed}').json()
 name = user_data[0].get('name')
 print(f'Employee {name} is done with tasks({len(completed)}/{num_of_tasks}):')
