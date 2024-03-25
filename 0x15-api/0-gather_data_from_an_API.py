e9f3c8125cbf47cddf16132a595191c532c9deba
aelbouaa <hbouaami@gmail.com>
2024-03-25 23:31:41 +0000

    check text errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """ fetsh data from JSON API"""
 import requests
-from sys import argv
+import sys
 
-def completed_todos():
+def main():
     """function that list of completed task using api json"""
-    data = f'https://jsonplaceholder.typicode.com/'
-    args = argv[1]
-    user_id = f'users?id={args}'
-    todo_list = f'todos?user_id={args}'
-    completed = f'{todo_list}?completed=True'
-    non_completed = f'{todo_list}?completed=False'
-    user_data = requests.get(f'{data}{user_id}').json()
-    todosData = requests.get(f'{data}{todo_list}').json()
-    todosDone = requests.get(f'{data}{completed}').json()
-    num_of_tasks = len(todosDone) + len(todosData)
-    name = user_data[0].get('name')
-    print(f'Employee {name} is done with tasks({len(todosDone)}/{num_of_tasks}):')
+    url = f'https://jsonplaceholder.typicode.com/'
+    args = sys.argv[1]
+    user = f'users?id={args}'
+    todoList = f'todos?user_id={args}'
+    completed = f'{todoList}&completed=true'
+    # non_completed = f'{todo_list}&completed=false'
+    userData = requests.get(f'{url}{user}').json()
+    todosData = requests.get(f'{url}{todoList}').json()
+    todosDone = requests.get(f'{url}{completed}').json()
+    sumOfTasks = len(todosDone) + len(todosData)
+    Name = userData[0].get('name')
+    print(f'Employee {Name} is done with tasks({len(todosDone)}/{sumOfTasks}):')
     for task in todosDone:
         print(f'\t ' + task.get('title'))
 
 
 if __name__ == "__main__":
-    completed_todos()
+    main()
