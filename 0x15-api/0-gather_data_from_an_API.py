e2038e582d8d34aa8dd9c6b5b367ab6aa8cc8fb5
aelbouaa <hbouaami@gmail.com>
2024-03-26 00:12:34 +0000

    which error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,26 +7,23 @@ import sys
 def main():
     """function that list of completed task using api json"""
 
-    if len(sys.argv) != 2:
-        return
-
-    url = f'https://jsonplaceholder.typicode.com/'
-    args = sys.argv[1]
-    user = f'users?id={args}'
-    todoList = f'todos?userId={args}'
-    completed = f'{todoList}&completed=true'
-    userData = requests.get(f'{url}{user}').json()
-    name = userData[0].get("name")
-
-    if name is not None:
-        todosData = requests.get('{}{}'.format(url, todoList)).json()
-        todosDone = requests.get('{}{}'.format(url, completed)).json()
-        lenDone = len(todosDone)
-        lenData = len(todosData)
-        print('Employee {} is done with tasks({:d}/{:d}):'.format(
-            name, lenDone, lenData))
-        for task in todosDone:
-            print('\t {}'.format(task.get("title")))
+    if len(sys.argv) > 1:
+        url = f'https://jsonplaceholder.typicode.com/'
+        args = sys.argv[1]
+        user = f'users?id={args}'
+        todoList = f'todos?userId={args}'
+        completed = f'{todoList}&completed=true'
+        userData = requests.get(f'{url}{user}').json()
+        name = userData[0].get("name")
+        if name is not None:
+            todosData = requests.get('{}{}'.format(url, todoList)).json()
+            todosDone = requests.get('{}{}'.format(url, completed)).json()
+            lenDone = len(todosDone)
+            lenData = len(todosData)
+            print('Employee {} is done with tasks({:d}/{:d}):'.format(
+                name, lenDone, lenData))
+            for task in todosDone:
+                print('\t {}'.format(task.get('title')))
 
 
 if __name__ == "__main__":
