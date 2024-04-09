2d3456145580253e5a9e528465de839ede5ebba9
aelbouaa <hbouaami@gmail.com>
2024-04-09 23:59:45 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that queries the
+Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+headers = {"User-Agent": "Custom User-Agent"}
+
+
+def top_ten(subreddit):
+    """method to retrieve the post from given dubreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print('None')
