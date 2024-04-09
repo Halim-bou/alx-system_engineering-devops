90c574f88eaa1c9f1ba480d2281f75d74747c75a
aelbouaa <hbouaami@gmail.com>
2024-04-09 23:22:00 +0000

    try with my user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,17 @@
 function that queries the Reddit API
 and returns the number of subscribers
 """
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """function that retrieve the subreddit fiven subscribers"""
-    headers = {'User-Agent': 'Custom User-Agent'}
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     try:
-        response = requests.get(url, headers=headers)
+        response = get(url, headers=headers, allow_redirects=False)
 
         if response.status_code != 200:
             return 0
